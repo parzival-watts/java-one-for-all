a438f77ccf61334c364b1527742981c46456bac7
Phiilip-Speed <phillip.speedsistemas@gmail.com>
2023-11-21 10:40:07 -0300

    Object orientation - Methods part-03 Return part-02

--- src/academy/devdojo/javaoneforall/javacore/Bintroductionmethods/domain/Calculator.java
+++ src/academy/devdojo/javaoneforall/javacore/Bintroductionmethods/domain/Calculator.java
@@ -2,15 +2,39 @@ package academy.devdojo.javaoneforall.javacore.Bintroductionmethods.domain;
 
 public class Calculator {
 
-    public void sumTwoNumbers(){
+    public void sumTwoNumbers() {
         System.out.println(10 + 10);
     }
-    public void subtractTwoNumbers(){
+
+    public void subtractTwoNumbers() {
         System.out.println(10 - 10);
     }
 
-    public void multiplyTwoNumbers(int num1, int num2){
+    public void multiplyTwoNumbers(int num1, int num2) {
         System.out.println(num1 * num2);
+    }
+
+    public double divideTwoNumbers(double num1, double num2) {
+        if (num1 == 0 && num2 == 0) {
+            return 0;
+        }
+        return num1 / num2;
+    }
+
+    public double divideTwoNumbers02(double num1, double num2) {
+        if (num1 != 0 && num2 != 0) {
+            return num1 / num2;
+        }
+        return 0;
+    }
+
+    public void printTwoNumbersDivided(double num1, double num2) {
+        if (num2 == 0) {
+            System.out.println("Can't divide by 0");
+            return;
+        }
+        System.out.println(num1 / num2);
+
     }
 
 }
