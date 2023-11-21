b95be608e4197bf16aa01a024b49328429eb18d3
Phiilip-Speed <phillip.speedsistemas@gmail.com>
2023-11-21 10:05:55 -0300

    Object orientation - Methods part-03 Return part-01

---
+++ src/academy/devdojo/javaoneforall/javacore/Bintroductionmethods/domain/Calculator.java
@@ -0,0 +1,16 @@
+package academy.devdojo.javaoneforall.javacore.Bintroductionmethods.domain;
+
+public class Calculator {
+
+    public void sumTwoNumbers(){
+        System.out.println(10 + 10);
+    }
+    public void subtractTwoNumbers(){
+        System.out.println(10 - 10);
+    }
+
+    public void multiplyTwoNumbers(int num1, int num2){
+        System.out.println(num1 * num2);
+    }
+
+}
